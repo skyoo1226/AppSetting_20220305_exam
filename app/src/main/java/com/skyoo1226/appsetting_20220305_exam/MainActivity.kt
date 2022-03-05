30c5c402ef6f354c4f9b82a4bebb1479c8ea017a
skyoo1226 <you1226@naver.com>
2022-03-05 20:56:35 +0900

    mipmap 아이콘(로고 등 이미지) 추가 : 보통 앞화면 또는 백그라운드 화면에 쓸 이미지(로고 등) - 모든 해상도 및 모양은 자동 생성 됨

--- app/src/main/java/com/skyoo1226/appsetting_20220305_exam/MainActivity.kt
+++ app/src/main/java/com/skyoo1226/appsetting_20220305_exam/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.skyoo.appsetting_20220305_exam
+package com.skyoo1226.appsetting_20220305_exam
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.skyoo.appsetting_20220305_exam.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
